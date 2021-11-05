54ff8695d09d174ccb52e8ce12386dcbb3b5281b
Iridero <45569252+Iridero@users.noreply.github.com>
2021-11-04 18:49:25 -0600

    Agregar archivos de proyecto.

---
+++ CrudTiendita/Models/Producto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace CrudTiendita.Models
+{
+    public partial class Producto
+    {
+        public int Idproductos { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public int Existecia { get; set; }
+    }
+}
---
+++ CrudTiendita/Repositories/Repository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CrudTiendita.Models;
+using Microsoft.EntityFrameworkCore;
+namespace CrudTiendita.Repositories
+{
+    public class Repository<T> where T:class
+    {
+        private tienditaContext context;
+        private DbSet<T> table=null;
+        public Repository(tienditaContext context)
+        {
+            this.context = context;
+            table = context.Set<T>()
+;        }
+
+        virtual public IEnumerable<T> GetAll()
+        {
+            
+        }
+    }
+}
